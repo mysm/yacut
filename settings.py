55563b1bb1886bc3f1ac895ae512f454ed158864
Михаил Мыслицкий <mysmster@yandex.ru>
2023-04-27 14:18:11 +0300

    add default values to settings.py for all values

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY", "nmZN2sGM394ntyKMcj014rGPrlJemvRd")
     TEMPLATE_FOLDER = os.path.abspath(os.getenv("TEMPLATE_FOLDER", "html"))
     STATIC_FOLDER = os.path.abspath(os.getenv("STATIC_FOLDER", "html"))
