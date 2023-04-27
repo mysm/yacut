79ab246c4d5c0c7eb18f21e52aaf725bddb0a4e2
Михаил Мыслицкий <mysmster@yandex.ru>
2023-04-27 14:11:15 +0300

    add default values for TEMPLATE_FOLDER, STATIC_FOLDER in env

--- settings.py
+++ settings.py
@@ -5,5 +5,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
-    TEMPLATE_FOLDER = os.path.abspath(os.getenv("TEMPLATE_FOLDER"))
-    STATIC_FOLDER = os.path.abspath(os.getenv("STATIC_FOLDER"))
+    TEMPLATE_FOLDER = os.path.abspath(os.getenv("TEMPLATE_FOLDER", "html"))
+    STATIC_FOLDER = os.path.abspath(os.getenv("STATIC_FOLDER", "html"))
